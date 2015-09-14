33010885c2f5e69e2b92965a60705d710ee30e57
el1414 <el1414@2kiso11.edu.fukushima-nct.ac.jp>
2015-09-14 11:22:43 +0900

    CoversionとCount_0 h27_9_14

--- test.py
+++ test.py
@@ -84,6 +84,25 @@ def getone(two):
 		onedata.append(twotoone(tmp))
 	return onedata
 
+def Conversion(list1):
+ i=0
+ list2=[]
+ for i in list1:
+  if i==2:
+   list2.append(1)
+  else :
+   list2.append(i)
+ return list2
+
+def Count_0(list1):
+ Count=0
+ i=0
+ for i in list1:
+  if i==0:
+   Count+=1
+ return Count
+
+
 data = getdata() #stage & stone two data
 onedata = getone(data) #stage & stone one data
 stage = onedata[0]
