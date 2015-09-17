86cf3f28c51c0fc81bb0bf3233c0c7e686f3c370
shouta <sofken.hukushima@gmail.com>
2015-09-17 14:51:16 +0900

    first tochu!

--- test.py
+++ test.py
@@ -250,6 +250,100 @@ def trimming(SIDE,STAGE):
 
 	return t_stage
 
+def First_search(stage_list):
+ #import math
+ Trimming_xy=[]
+ Trimming_size=9
+ center_xy=int(Trimming_size/2)
+ Number_0=Count_0(stage_list)
+ Standard_Barrier_Rate=6
+ stage_len=len(stage_list)
+ stage_size=math.sqrt(stage_len)
+ Barrier_Rate=(stage_len-Number_0)/float(stage_len)*100
+ flag=-1#0:Center 1:Corner
+ most_0=Dist(stage_list)
+ if Barrier_Rate==0:
+  return [[center_xy,center_xy]]
+ if(Barrier_Rate>Standard_Barrier_Rate):
+  flag=0
+ else:
+  flag=1
+ Trimming_xy=[]
+ left=int(math.ceil(stage_size/2)-1)
+ right=int(math.floor(stage_size/2))
+ center=[[left,left],[left,right],[right,right],[right,left]]
+ center_around=[[left-center_xy,left-center_xy],[left-center_xy,right+center_xy],[right+center_xy,right+center_xy],[right+center_xy,left-center_xy]]
+ side=[[],[],[],[]]
+ side[0]=[[center_xy,center_xy],[center_xy,left-center_xy],[left-center_xy,left-center_xy],[left-center_xy,center_xy]]
+ side[1]=[[center_xy,right+left-center_xy],[left-center_xy,right+left-center_xy],[left-center_xy,right+center_xy],[center_xy,right+center_xy]]
+ side[2]=[[right+left-center_xy,right+left-center_xy],[right+left-center_xy,right+center_xy],[right+center_xy,right+center_xy],[right+center_xy,right+left-center_xy]]
+ side[3]=[[right+left-center_xy,center_xy],[right+center_xy,center_xy],[right+center_xy,left-center_xy],[right+left-center_xy,left-center_xy]]
+ Trimming_yx=[]
+ if flag==0:
+  i=0
+  while i<4:
+   Trimming_yx.append(center[most_0[i]])
+   i+=1
+  i=0
+  while i<4:
+   Trimming_yx.append(center_around[most_0[i]])
+   i+=1
+ if flag==1:
+  i=0
+  while i<4:
+   Trimming_yx.extend(side[most_0[i]])
+   i+=1
+ return Trimming_yx
+
+def Dist(list1):
+ #import math
+ stage_size=int(math.sqrt(len(list1)))
+ cut_stage=CUT(list1,stage_size)
+ center_left=int(math.ceil(stage_size/2)-1)
+ center_right=int(math.floor(stage_size/2))
+ Number_0=[0,0,0,0]
+ most_direct=[]
+ i=0
+ while i<stage_size:
+  j=0
+  while j<stage_size:
+   if cut_stage[i][j]==0:
+    if i<=center_left and j<=center_left:
+     Number_0[0]+=1
+    if i>=center_right and j<=center_left:
+     Number_0[1]+=1
+    if i>=center_right and j>=center_right:
+     Number_0[2]+=1
+    if i<=center_left and j>=center_right:
+     Number_0[3]+=1
+   j+=1
+  i+=1 
+ i=0
+ j=0
+ while j<4:
+  i=0
+  most_number=0
+  most=Number_0[0]
+  while i<4: 
+   if most<Number_0[i]:
+    most=Number_0[i]
+    most_number=i
+   i+=1
+  most_direct.append(most_number)
+  Number_0[most_number]=0
+  j+=1
+ return most_direct
+
+def CUT(list1,cut_size):
+ Cut_list=[]
+ i=0
+ k=cut_size
+ while k<=cut_size*cut_size:
+  Cut_list.append(list1[i:k])
+  i+=cut_size
+  k+=cut_size
+ return Cut_list
+
 #View Stage for DEBUG
 def viewstage(twostage):
 	for i in range(32):
@@ -259,6 +353,10 @@ def viewstage(twostage):
 		print(line)
 
 #stone & stage reading
+#data     = two stone
+#onedata  = one stone
+#twostage = two stage
+#onestage = one stage
 data = getdata() #stage & stone two data
 onedata = []
 for i in range(len(data)):
@@ -269,8 +367,10 @@ del data[0]
 onestage = getone(twostage)
 del onedata[0]
 
-stonelen = len(data)
 #put stone & output
+stonelen = len(data)
+firstcoor = First_search(onestage)
+print(firstcoor)
 for n in range(stonelen):
 	#tromming datas
 	side=Find_side(twostage)
@@ -288,4 +388,4 @@ for n in range(stonelen):
 	rnd = random.randint(0,sidelen-1)
 	action = test(IF_stage,IF_stone,Main_code,tristage[0],stone_now)
 	putstone(twostage,data[n],[side[rnd][1][0]+4,side[rnd][1][1]+4],side[rnd][0],act[action])
-	viewstage(twostage)
+	#viewstage(twostage)
